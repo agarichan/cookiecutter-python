42c1ba66999b3c3b941613801d29f301f148a3bf
agarichan <ag4r1chan@gmail.com>
2022-11-29 15:13:25 +0900

    flake8のバージョン変更

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.call(['poetry', 'add', '-D', 'pytest', 'pytest-cov', 'pytest-asyncio', 'pyright', 'black', 'flake8=5.0.4', 'isort', 'poethepoet', 'pyproject-flake8', 'notebook'])
+subprocess.call(['poetry', 'add', '-D', 'pytest', 'pytest-cov', 'pytest-asyncio', 'pyright', 'black', 'flake8', 'isort', 'poethepoet', 'pyproject-flake8', 'notebook'])
 subprocess.call(['poetry', 'install'])
